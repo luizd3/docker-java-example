8228788ca3045292f5cb4315e2e2be8731863e8f
Luiz Dorça <luizdorca@gmail.com>
2023-05-04 20:17:07 -0300

    Adicionadas anotações JPA na classe User

--- src/main/java/com/lodn/dockerjava/controllers/User.java
+++ src/main/java/com/lodn/dockerjava/controllers/User.java
@@ -1,9 +1,17 @@
 package com.lodn.dockerjava.controllers;
 
+import jakarta.persistence.*;
+
 import java.util.UUID;
 
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+
+    @Column(nullable = false)
     private String name;
 
     public UUID getId() {
