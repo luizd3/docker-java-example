cadeb786d5d5540a54e5b9598e7cb7dd348aeaa4
Luiz Dorça <luizdorca@gmail.com>
2023-05-08 23:56:36 -0300

    Adicionado método POST

--- src/main/java/com/lodn/dockerjava/controllers/HelloController.java
+++ src/main/java/com/lodn/dockerjava/controllers/HelloController.java
@@ -3,9 +3,7 @@ package com.lodn.dockerjava.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +23,12 @@ public class HelloController {
     @GetMapping("all")
     public ResponseEntity<List<User>> findAll() {
         return new ResponseEntity<>(usersRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user) {
+        User createdUser = usersRepository.save(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
     }
 
 }
